6247254a17afc24337484a82e7129603aa0cc604
Fabio Gentile <gentilef.fabio@gmail.com>
2023-11-10 18:12:00 +0100

    controller home index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AdvertRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(AdvertRepository $advert): Response
     {
+        $electricCars = $advert->findByCriteriaExpression('essence', 'fuelType', 3);
         return $this->render('home/index.html.twig', [
-
+            'lastAdverts' => $advert->findBy([], ['createdAt' => 'DESC'], 3),
+            'electricCars' =>  $electricCars
         ]);
     }
 }
